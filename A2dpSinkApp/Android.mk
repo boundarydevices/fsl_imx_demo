2392209f2984b96de5dd6488cb9af0f23896e10e
Haoran Wang <b50027@freescale.com>
2015-01-19 17:30:47 +0800

    MA-5945 Added A2dpSinkApp basic source codes
    Use AudioRecord to get A2dp Audio data from Source.
    Use AudioTracker to play the data.
    Establish basic Bluetooth A2dp Sink display and control
    in the app.
    
    Signed-off-by: Haoran Wang <b50027@freescale.com>

---
+++ A2dpSinkApp/Android.mk
@@ -0,0 +1,16 @@
+ifeq ($(BOARD_USE_AR3K_BLUETOOTH),true)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+
+LOCAL_PACKAGE_NAME := A2dpSinkApp
+
+include $(BUILD_PACKAGE)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+
