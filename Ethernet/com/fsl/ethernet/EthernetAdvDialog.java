7304bacd0ea10e944b2504f44a838bf2dc5930d9
Haoran Wang <b50027@freescale.com>
2015-06-03 18:05:39 +0800

    MA-6531-1 ethernet app: Add proxy enable in proxy setting
    Added proxy enable checkbox in proxy setting activity.
    When it is checked user can setup the proxy info.
    If it is unchecked the input TextEdit will be gone.
    
    Signed-off-by: Haoran Wang <b50027@freescale.com>

--- Ethernet/com/fsl/ethernet/EthernetAdvDialog.java
+++ Ethernet/com/fsl/ethernet/EthernetAdvDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Freescale Semiconductor, Inc.
+ * Copyright (C) 2014-2015 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,9 @@ public class EthernetAdvDialog extends AlertDialog implements DialogInterface.On
     private EditText mProxyIp;
     private EditText mProxyPort;
     private EditText mProxyExclusionList;
+    private LinearLayout mConfigWindow;
     private LinkProperties mLinkProperties;
+    private CheckBox mProxyEnableCheckBox;
     private ProxyInfo mHttpProxy;
     private static ConnectivityManager sConnectivityManager = null;
 
@@ -62,6 +64,22 @@ public class EthernetAdvDialog extends AlertDialog implements DialogInterface.On
         mProxyIp = (EditText)mView.findViewById(R.id.proxy_address_edit);
         mProxyPort = (EditText)mView.findViewById(R.id.proxy_port_edit);
         mProxyExclusionList = (EditText)mView.findViewById(R.id.proxy_exclusionlist);
+        mProxyEnableCheckBox = (CheckBox)mView.findViewById(R.id.proxy_enable_checkbox);
+        mConfigWindow = (LinearLayout)mView.findViewById(R.id.enterprise_wrapper);
+        mProxyEnableCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mConfigWindow.setVisibility(View.VISIBLE);
+                } else {
+                    mConfigWindow.setVisibility(View.GONE);
+                }
+                mProxyIp.setEnabled(isChecked);
+                mProxyPort.setEnabled(isChecked);
+                mProxyExclusionList.setEnabled(isChecked);
+            }
+        });
+
 
         this.setInverseBackgroundForced(true);
         this.setButton(BUTTON_POSITIVE, context.getText(R.string.menu_save), this);
@@ -69,6 +87,13 @@ public class EthernetAdvDialog extends AlertDialog implements DialogInterface.On
         mProxyIp.setText(mEthEnabler.getManager().getSharedPreProxyAddress(),TextView.BufferType.EDITABLE);
         mProxyPort.setText(mEthEnabler.getManager().getSharedPreProxyPort(),TextView.BufferType.EDITABLE);
         mProxyExclusionList.setText(mEthEnabler.getManager().getSharedPreProxyExclusionList(),TextView.BufferType.EDITABLE);
+        if (mEthEnabler.getManager().getSharedPreProxyAddress() == "") {
+            mProxyEnableCheckBox.setChecked(false);
+            mConfigWindow.setVisibility(View.GONE);
+        } else {
+            mProxyEnableCheckBox.setChecked(true);
+            mConfigWindow.setVisibility(View.VISIBLE);
+        }
         this.setInverseBackgroundForced(true);
         this.setButton(BUTTON_POSITIVE, context.getText(R.string.menu_save), this);
         this.setButton(BUTTON_NEGATIVE, context.getText(R.string.menu_cancel), this);
@@ -83,12 +108,20 @@ public class EthernetAdvDialog extends AlertDialog implements DialogInterface.On
             info.setConnectMode(mEthEnabler.getManager().getSharedPreMode());
             info.setIpAddress(mEthEnabler.getManager().getSharedPreIpAddress());
             info.setDnsAddr(mEthEnabler.getManager().getSharedPreDnsAddress());
-            info.setProxyAddr(mProxyIp.getText().toString());
-            info.setProxyPort(mProxyPort.getText().toString());
-            info.setProxyExclusionList(mProxyExclusionList.getText().toString());
+
+            if (mProxyEnableCheckBox.isChecked()) {
+                info.setProxyAddr(mProxyIp.getText().toString());
+                info.setProxyPort(mProxyPort.getText().toString());
+                info.setProxyExclusionList(mProxyExclusionList.getText().toString());
+            } else {
+                info.setProxyAddr("");
+                info.setProxyPort("");
+                info.setProxyExclusionList("");
+            }
+
             mEthEnabler.getManager().updateDevInfo(info);
             mEthEnabler.getManager().setProxy();
-        }else{
+        } else {
            Toast.makeText(this.getContext(),R.string.show_connect_ethernet,Toast.LENGTH_SHORT).show();
         }
     }
