50f912e4c326c363e20d1650dc2f448384cbb9ec
Wenbo Zhu <b52619@freescale.com>
2015-07-22 16:56:39 +0800

    MA-6914 when auto sx/dq diaplay without hdmi, the "enable passthrough" isn't be show on AudioRoute application. and make the apk support sabresd dq.
      add passthrough.xml, nodevice.xml and spdifin.xml in layout, and add Judgment statement to
    distinguish different display.
    
    Signed-off-by: Wenbo Zhu <b52619@freescale.com>

--- AudioRoute/src/com/example/audioroute/AudioRoute.java
+++ AudioRoute/src/com/example/audioroute/AudioRoute.java
@@ -17,7 +17,7 @@
 
 package com.example.audioroute;
 
-
+import java.io.File;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -53,24 +53,90 @@ public class AudioRoute extends Activity {
         // Create and attach the view that is responsible for painting.
 
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        File fileHdmi=new File("proc/asound/imxhdmisoc");
+        File fileSpdif=new File("proc/asound/imxspdif");
+        if(fileHdmi.exists()&&!fileSpdif.exists())
+        {
+         setContentView(R.layout.passthrough);
+         mCheckBoxPassthrough = (CheckBox)findViewById(R.id.CheckPassthrough);
+         mTextViewOutputChoose = (TextView) findViewById(R.id.AudioOutputText);
+        }
+        else if(!fileHdmi.exists()&&fileSpdif.exists())
+        {
+         setContentView(R.layout.spdifin);
+         mSpdifIn = (CheckBox)findViewById(R.id.spdif_in);
+         mCommand = (TextView) findViewById(R.id.commandText);
+        }
+        else if(fileHdmi.exists()&&fileSpdif.exists())
+        {
+         setContentView(R.layout.main);
+         mCheckBoxPassthrough = (CheckBox)findViewById(R.id.CheckPassthrough);
+         mSpdifIn = (CheckBox)findViewById(R.id.spdif_in);
+         mCommand = (TextView) findViewById(R.id.commandText);
+         mTextViewOutputChoose = (TextView) findViewById(R.id.AudioOutputText);
+        }
+        else 
+        {
+         setContentView(R.layout.nodevice);
+         Log.d(TAG,"no device connected");
+        }
 
-        setContentView(R.layout.main);
-        mCommand = (TextView) findViewById(R.id.commandText);
-        mSpdifIn = (CheckBox)findViewById(R.id.spdif_in);
-        mTextViewOutputChoose = (TextView) findViewById(R.id.AudioOutputText);
-        mCheckBoxPassthrough = (CheckBox)findViewById(R.id.CheckPassthrough);
+       if(fileHdmi.exists()&&!fileSpdif.exists())
+        {
+         mTextViewOutputChoose.setText(getResources().getString(R.string.output_choose));
+         mCheckBoxPassthrough.setText(getResources().getString(R.string.Enable_Passthrough));
+        }
+      else if(!fileHdmi.exists()&&fileSpdif.exists())
+        {
+         mCommand.setText(getResources().getString(R.string.device_choose));
+         mSpdifIn.setText(getResources().getString(R.string.Enable_SPDIF_In));
+        }
+      else if(fileHdmi.exists()&&fileSpdif.exists())
+        {
+         mTextViewOutputChoose.setText(getResources().getString(R.string.output_choose));
+         mCheckBoxPassthrough.setText(getResources().getString(R.string.Enable_Passthrough));
+         mCommand.setText(getResources().getString(R.string.device_choose));
+         mSpdifIn.setText(getResources().getString(R.string.Enable_SPDIF_In));
+        }
+      else
+          Log.d(TAG,"no device connected");
 
-
-        mCommand.setText(getResources().getString(R.string.device_choose));
-
-        mSpdifIn.setText(getResources().getString(R.string.Enable_SPDIF_In));
-
-        mTextViewOutputChoose.setText(getResources().getString(R.string.output_choose));
-        mCheckBoxPassthrough.setText(getResources().getString(R.string.Enable_Passthrough));
-
-        if (AudioSystem.getDeviceConnectionState(AudioSystem.DEVICE_IN_AUX_DIGITAL, "") == AudioSystem.DEVICE_STATE_AVAILABLE)
-            mSpdifIn.setChecked(true);
+      if(fileHdmi.exists()&&!fileSpdif.exists())
+        {
+        String value = SystemProperties.get("persist.audio.pass.through");
+        Log.d(TAG,"property persist.audio.pass.through is " + value);
+        if(value.equals("2000"))
+            mCheckBoxPassthrough.setChecked(true);
         else
+            mCheckBoxPassthrough.setChecked(false);
+
+        mCheckBoxPassthrough.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, "CheckBox Passthrough is clicked");
+                SystemProperties.set("persist.audio.pass.through",mCheckBoxPassthrough.isChecked() ? "2000" : "0" );
+            }
+        });
+        }
+      else if (!fileHdmi.exists()&&fileSpdif.exists())
+         {
+         if (AudioSystem.getDeviceConnectionState(AudioSystem.DEVICE_IN_AUX_DIGITAL, "") == AudioSystem.DEVICE_STATE_AVAILABLE)
+            mSpdifIn.setChecked(true);
+          else
+            mSpdifIn.setChecked(false);
+
+        mSpdifIn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, "SPDIF IN is clicked");
+
+                setupAudioRoute();
+            }
+        });
+         }
+      else if(fileHdmi.exists()&&fileSpdif.exists())
+         {
+         if (AudioSystem.getDeviceConnectionState(AudioSystem.DEVICE_IN_AUX_DIGITAL, "") == AudioSystem.DEVICE_STATE_AVAILABLE)
+            mSpdifIn.setChecked(true);
+          else
             mSpdifIn.setChecked(false);
 
         mSpdifIn.setOnClickListener(new OnClickListener() {
@@ -94,6 +160,9 @@ public class AudioRoute extends Activity {
                 SystemProperties.set("persist.audio.pass.through",mCheckBoxPassthrough.isChecked() ? "2000" : "0" );
             }
         });
+       }
+      else
+      Log.d(TAG, "no device connected and can't do some operation");
 
 
     }
