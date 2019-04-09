563b0f7faa04eabe0d8e8d23c97b91cd7d760d4a
brain <brain.han@nxp.com>
2019-04-09 16:21:19 +0800

    MA-14585 Solve icon, ui and crash issue of the Directaudioplayer.
    The Icon is changed as the logo of NXP, and the UI is
    changed to the play and pause button. Solve the crash
    issue when select another music to play at the playing
    status.
    
    Change-Id: Icd1869d25d9117583c5d48f81f1248f0fb91f655
    Signed-off-by: brain <brain.han@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8123
    Reviewed-by: fang hui <hui.fang@nxp.com>

--- DirectAudioPlayer/java/com/nxp/directaudioplayer/MainActivity.java
+++ DirectAudioPlayer/java/com/nxp/directaudioplayer/MainActivity.java
@@ -19,7 +19,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.ListView;
 import android.widget.TextView;
 import java.io.BufferedReader;
@@ -33,7 +34,8 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -44,10 +46,17 @@ public class MainActivity extends Activity {
     int positionTime=0;
     ListView mFileList;
     TextView mSelectedFfileNameText;
-    TextView mTime;
     String mSelectedFileName;
     File mRootdDirectory;
     ThreadPlay threadPlay = new ThreadPlay();
+
+    private TextView tv_start;
+    private TextView tv_end;
+    private SeekBar seekbar;
+    private ImageButton imageButton;
+    private Timer timer;
+
+    private boolean isSeekBarChanging;
     private static String TAG = "AudioTrack";
     public final static int PERMISSION_REQUESTCODE = 1;
     public final static String LPA_ENABLE_PROPERTY = "vendor.audio.lpa.enable";
@@ -107,61 +116,85 @@ public class MainActivity extends Activity {
         }
     }
 
-    View.OnClickListener playclick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Log.i(TAG, "play click");
-            int playstate =0;
-            if(threadPlay.mTrack!=null){
-                playstate = threadPlay.mTrack.getPlayState();
-            }
-            if(playstate==0){
-                threadPlay.start();
-                avoid_RedundantClickCrash = true;
-                mHandle.postDelayed(mAutoTimerRunnable,AUTO_UPDATE_TIMER);
-            }
-            if(playstate== AudioTrack.PLAYSTATE_PAUSED){
-                threadPlay.mTrack.play();
-            }
-        }
-    };
-
-    private Runnable mAutoTimerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Message msg = new Message();
-            msg.arg1 = positionTime;
-            mHandle.sendMessage(msg);
-            mHandle.postDelayed(this,AUTO_UPDATE_TIMER);
-        }
-    };
-
-    private Handler mHandle = new Handler(){
-        public void handleMessage(Message msg){
-            super.handleMessage(msg);
-            mTime.setText("Have played "+new String(String.valueOf(msg.arg1))+" seconds");
-        }
-    };
-    View.OnClickListener pauselick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Log.i(TAG, "pause click");
-            threadPlay.mTrack.pause();
-        }
-    };
-
     private void initView() {
         mFileList = (ListView) findViewById(R.id.listSongs);
-        Button mPlayButton = (Button) findViewById(R.id.Play);
-        Button mPauseButton = findViewById(R.id.Pause);
         mSelectedFfileNameText = (TextView) findViewById(R.id.Display_SelectedFile);
-        mTime = findViewById(R.id.Time);
-        mPlayButton.setOnClickListener(playclick);
-        mPauseButton.setOnClickListener(pauselick);
         mRootdDirectory = Environment.getExternalStorageDirectory();
         mSelectedFfileNameText.setText("");
-        mTime.setText("");
         listFiles();
+
+        initSeekbar();
+
+        imageButton = (ImageButton) findViewById(R.id.bt_media);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isPlayOrPause();
+            }
+        });
+    }
+
+    public void isPlayOrPause() {
+        int playstate =0;
+        if(threadPlay.mTrack!=null){
+            playstate = threadPlay.mTrack.getPlayState();
+        }
+        if(playstate == 0){
+            Log.i(TAG, "play click");
+            initSeekbar();
+            threadPlay.start();
+            avoid_RedundantClickCrash = true;
+            imageButton.setImageResource(android.R.drawable.ic_media_pause);
+            seekbar.setMax(threadPlay.getDuration());
+            getProgress();
+        }
+        if(playstate== 2){
+            Log.i(TAG, "play click");
+            threadPlay.mTrack.play();
+            imageButton.setImageResource(android.R.drawable.ic_media_pause);
+        }
+        if(playstate== 3){
+            Log.i(TAG, "pause click");
+            threadPlay.mTrack.pause();
+            imageButton.setImageResource(android.R.drawable.ic_media_play);
+        }
+    }
+
+    private  void initSeekbar(){
+        tv_start = (TextView) findViewById(R.id.tv_start);
+        tv_end = (TextView) findViewById(R.id.tv_end);
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv_start.setText(calculateTime(getCurrentPosition()));
+                tv_end.setText(calculateTime(threadPlay.getDuration()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                isSeekBarChanging = true;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                isSeekBarChanging = false;
+                tv_start.setText(calculateTime(getCurrentPosition()));
+            }
+        });
+    }
+
+    private void getProgress() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(!isSeekBarChanging) {
+                    seekbar.setProgress(getCurrentPosition());
+                }
+            }
+        },0,50);
     }
 
     class Filter implements FilenameFilter {
@@ -193,8 +226,12 @@ public class MainActivity extends Activity {
                 if(cnt>0){
                     threadPlay.isPlaying = false;
                     synchronized (threadPlay.mTrack){
-                        threadPlay.mTrack.stop();
-                        threadPlay.mTrack.release();
+                        if(threadPlay.mTrack.getPlayState() == AudioTrack.PLAYSTATE_STOPPED){
+                            initView();
+                        }else{
+                            threadPlay.mTrack.pause();
+                            threadPlay.mTrack.flush();
+                        }
                     }
                     threadPlay = new ThreadPlay();
                 }
@@ -219,7 +256,7 @@ public class MainActivity extends Activity {
         byte[] mediaBuffer;
         float[] mediaBufferFloat;
         int minBufSize;
-        int frameSize;
+        int maxSize;
 
         public void setFile(String filename) {
             this.mFileName = filename;
@@ -265,9 +302,6 @@ public class MainActivity extends Activity {
                 Log.e(TAG, "unsupported bits " + bits + ", treat as 16 bits");
                 return;
             }
-
-            frameSize = chans * bits/8;
-
             AudioManager mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
             if (bits == 16)
                 mAudioManager.setParameters("pcm_bit=16");
@@ -331,9 +365,9 @@ public class MainActivity extends Activity {
                 if(state == AudioTrack.PLAYSTATE_PAUSED) {
                     Log.v(TAG, "PLAYSTATE_PAUSED");
                     try {
-                      Thread.sleep(10);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
-                      e.printStackTrace();
+                        e.printStackTrace();
                     }
                     continue;
                 }
@@ -367,18 +401,17 @@ public class MainActivity extends Activity {
 
                     written += ret;
                     if(written < read) {
-                       toWrite = read - written;
+                        toWrite = read - written;
 
-                       try {
-                           Thread.sleep(10);
-                       } catch (InterruptedException e) {
-                          e.printStackTrace();
-                       }
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 } // write cycle
 
                 totalFeedSize += written;
-                positionTime = (int)totalFeedSize / frameSize / rate;
             } // while (isPlaying)
 
             if(mLPA == 1) {
@@ -392,16 +425,17 @@ public class MainActivity extends Activity {
                 long restTimeMs = restFrame*1000/rate;
 
                 Log.i(TAG, "Done feeding data, total frames " + totalFrame + ", played frames " + playedFrame +
-                      ", rest frames " + restFrame + ", rest ms " + restTimeMs + ", frame size " + frameSize);
+                        ", rest frames " + restFrame + ", rest ms " + restTimeMs + ", frame size " + frameSize);
 
                 try {
-                  Thread.sleep(restTimeMs);
+                    Thread.sleep(restTimeMs);
                 } catch (InterruptedException e) {
-                  e.printStackTrace();
+                    e.printStackTrace();
                 }
             }
 
             Log.i(TAG, "Done playing");
+            imageButton.setImageResource(android.R.drawable.ic_media_play);
 
             try {
                 minputStream.close();
@@ -424,7 +458,7 @@ public class MainActivity extends Activity {
             return array;
         }
 
-    public void paserWAVheader() {
+        public void paserWAVheader() {
             byte[] head = new byte[44];
             try {
                 read = minputStream.read(head);
@@ -448,7 +482,82 @@ public class MainActivity extends Activity {
                     ((int) (head[24 + 3] & 0x0FF) << 24);
             bits = head[34];
             chans = head[22];
+            int byteRate = (int) (head[28] & 0x0FF) +
+                    ((int) (head[28 + 1] & 0x0FF) << 8) +
+                    ((int) (head[28 + 2] & 0x0FF) << 16) +
+                    ((int) (head[28 + 3] & 0x0FF) << 24);
+            int waveSize = (int) (head[40] & 0x0FF) +
+                    ((int) (head[40 + 1] & 0x0FF) << 8) +
+                    ((int) (head[40 + 2] & 0x0FF) << 16) +
+                    ((int) (head[40 + 3] & 0x0FF) << 24);
+            maxSize = waveSize / byteRate;
+            Log.i(TAG, "The time of the music: " + maxSize + "(s)");
             Log.i(TAG, "rate " + rate + ", bits " + bits + ", chans " + chans);
         }
+
+        public int getDuration(){
+            paserWAVheader();
+            return  maxSize;
+        }
+    }
+
+    public String calculateTime(int time) {
+        int minute;
+        int second;
+
+        if (time >= 60) {
+            minute = time / 60;
+            second = time % 60;
+
+            if(minute>=0&&minute<10)
+            {
+                if(second>=0&&second<10)
+                {
+                    return "0"+minute+":"+"0"+second;
+                }else
+                {
+                    return "0"+minute+":"+second;
+                }
+
+            }else
+            {
+                if(second>=0&&second<10)
+                {
+                    return minute+":"+"0"+second;
+                }else
+                {
+                    return minute+":"+second;
+                }
+            }
+
+        } else if (time < 60) {
+            second = time;
+            if(second>=0&&second<10)
+            {
+                return "00:"+"0"+second;
+            }else
+            {
+                return "00:" + second;
+            }
+
+        }
+        return null;
+    }
+
+    public int getCurrentPosition() {
+        int CurrentPosition = 0;
+        if (threadPlay != null) {
+            if (threadPlay.mTrack != null) {
+                synchronized (threadPlay.mTrack) {
+                    if (threadPlay.mTrack.getPlayState() == 0|| threadPlay.mTrack.getPlayState() == 2||
+                            threadPlay.mTrack.getPlayState() == 3 ) {
+                        CurrentPosition = threadPlay.mTrack.getPlaybackHeadPosition() / threadPlay.rate;
+                    }else{
+                        CurrentPosition = 0;
+                    }
+                }
+            }
+        }
+        return CurrentPosition;
     }
 }
