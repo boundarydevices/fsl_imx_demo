471e322647d1bbbf9ef790ddbbf6c6bd5685fbfb
Jindong <jindong.yue@nxp.com>
2019-08-02 09:55:04 +0800

    MA-15284 ethernet: fix build error of emulator and vts
    Build emulator:
      lunch aosp_x86_64-eng
      make
      Error log:
        Either LOCAL_SDK VERSION or LOCAL_PRIVATE_PLATFORM_APIS can be defined. Other modules of this project
        use LOCAL_SDK_VERSION. If change to LOCAL_PRIVATE_PLATFORM_APIS the project can be built.
    
    Build vts:
      lunch aosp_arm64-userdebug
      make vts
      Error log(if not defined LOCAL_PRIVATE_PLATFORM_APIS):
        ethernet: Must specify LOCAL_SDK_VERSION or LOCAL_PRIVATE_PLATFORM_APIS
    
    When build these two targets, no need to include this makefile.
    
    Change-Id: I2e6d84cb5b0e7f6ebd6aa4dd63a7d1e69fed8b75
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9666
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    (cherry picked from commit 113134cd04e1b97d93b2aac130d5bfde14d696d5)
    Reviewed-on: http://androidsource.ap.freescale.net/project/9676

--- Ethernet/Android.mk
+++ Ethernet/Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(PRODUCT_MANUFACTURER),freescale)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -17,3 +18,4 @@ include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
