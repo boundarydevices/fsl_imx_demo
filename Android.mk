dca3848bfe6ff8308f939159ccc0ce521a86ab7a
dandan.sun <dandan.sun@nxp.com>
2019-08-08 18:04:58 +0800

    MA-15263-1 convert Android.mk to Android.bp
    convert all the Android.mk to Android.bp under vendor/nxp-opensource/fsl_imx_demo
    remove "LOCAL_SDK_VERSION := current" in some apks for build error
    
    no need to add resource_dirs: ["res"]
    
    Change-Id: I85f67c3f92e4a38be42f8c72616ba93bce356440
    Signed-off-by: dandan.sun <dandan.sun@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9713
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- Android.mk
+++
@@ -1,2 +0,0 @@
-#Copyright (c) 2011 Freescale Semiconductor, Inc.
-include $(call all-subdir-makefiles)
---
+++ Ethernet/ethernet.go
@@ -0,0 +1,49 @@
+// Copyright 2019 NXP
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ethernet
+
+import (
+        "android/soong/android"
+        "android/soong/cc"
+        "github.com/google/blueprint/proptools"
+)
+
+func init() {
+    android.RegisterModuleType("ethernet_defaults", ethernetDefaultsFactory)
+}
+
+func ethernetDefaultsFactory() (android.Module) {
+    module := cc.DefaultsFactory()
+    android.AddLoadHook(module, ethernetDefaults)
+    return module
+}
+
+func ethernetDefaults(ctx android.LoadHookContext) {
+    type props struct {
+        Target struct {
+                Android struct {
+                        Enabled *bool
+                }
+        }
+    }
+
+    p := &props{}
+    if ctx.Config().VendorConfig("IMXPLUGIN").String("PRODUCT_MANUFACTURER") == "freescale" {
+        p.Target.Android.Enabled = proptools.BoolPtr(true)
+    }  else {
+        p.Target.Android.Enabled = proptools.BoolPtr(false)
+    }
+    ctx.AppendProperties(p)
+}
