6087340803395dbc1287e9922f493ff804355b07
Gary Bisson <gary.bisson@boundarydevices.com>
2020-07-16 22:52:48 +0200

    FSLOta: allow to force URL of the package to download
    This skips the build.prop check, only care about downloading the package
    without asking user to confirm.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- FSLOta/src/com/fsl/android/ota/OTAServerConfig.java
+++ FSLOta/src/com/fsl/android/ota/OTAServerConfig.java
@@ -107,6 +107,15 @@ public class OTAServerConfig {
         return true;
     }
 
+    public void setUpdatePackageURL(String url)
+    {
+        try {
+            updatePackageURL = new URL(url);
+        } catch (Exception e) {
+            Log.e(TAG, "Wrong URL format " + url);
+        }
+    }
+
     void defaultConfigure(String productname) throws MalformedURLException
     {
         product = productname;
--- FSLOta/src/com/fsl/android/ota/OTAServerManager.java
+++ FSLOta/src/com/fsl/android/ota/OTAServerManager.java
@@ -114,6 +114,10 @@ public class OTAServerManager {
         mUpdateEngine = new UpdateEngine();
     }
 
+    public void setUpdatePackageURL(String updatePackageUrl) {
+        mConfig.setUpdatePackageURL(updatePackageUrl);
+    }
+
     public OTAStateChangeListener getmListener() {
         return mListener;
     }
@@ -282,7 +286,8 @@ public class OTAServerManager {
 
         Log.v(TAG, "startDownloadUpgradePackage()");
 
-        if (checkURLOK(mConfig.getPackageURL()) == false) {
+        if (!mConfig.getPackageURL().toString().contains("file://") &&
+                (checkURLOK(mConfig.getPackageURL()) == false)) {
             if (this.mListener != null)
                 reportDownloadError(OTAStateChangeListener.ERROR_CANNOT_FIND_SERVER);
             return;
