ab87976769a746185424ba7ff2d20e60d265b1b5
Gary Bisson <gary.bisson@boundarydevices.com>
2020-07-21 11:43:54 +0200

    FSLOta: OTAServerManager: make sure the file is properly sync'd
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- FSLOta/src/com/fsl/android/ota/OTAServerManager.java
+++ FSLOta/src/com/fsl/android/ota/OTAServerManager.java
@@ -328,7 +328,7 @@ public class OTAServerManager {
             lengthOfFile = conexion.getContentLength();
             // download the file
             InputStream input = new BufferedInputStream(url.openStream());
-            OutputStream output = new FileOutputStream(targetFile);
+            FileOutputStream output = new FileOutputStream(targetFile);
 
             Log.d(TAG, "file size:" + lengthOfFile);
             byte data[] = new byte[100 * 1024];
@@ -342,6 +342,7 @@ public class OTAServerManager {
             }
 
             output.flush();
+            output.getFD().sync();
             output.close();
             input.close();
             if (this.mListener != null && !mStop)
