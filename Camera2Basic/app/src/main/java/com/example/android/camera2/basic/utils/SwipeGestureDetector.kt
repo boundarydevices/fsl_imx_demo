c1368e937138904b127f1858dbb3671099fa418f
Anle Pan <anle.pan@nxp.com>
2022-08-24 16:19:16 +0000

    MA-20560 Fix the compile error in Camera2Basic
    android 13 doesn't package Camera2Basic app
    because of the compile error,
    fix the type mismatch error
    
    Change-Id: I6df4a8b76048f5b10ebe05ae530620c2c0f12fc8
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/19779
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- Camera2Basic/app/src/main/java/com/example/android/camera2/basic/utils/SwipeGestureDetector.kt
+++ Camera2Basic/app/src/main/java/com/example/android/camera2/basic/utils/SwipeGestureDetector.kt
@@ -11,7 +11,7 @@ class SwipeGestureDetector : GestureDetector.SimpleOnGestureListener() {
 
     var swipeCallback: SwipeCallback? = null
 
-    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
         if (e1 == null || e2 == null) return super.onFling(e1, e2, velocityX, velocityY)
         val deltaX = e1.x - e2.x
         val deltaXAbs = abs(deltaX)
