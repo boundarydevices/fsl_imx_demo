e2f077928ab51b2c318a9f51af987e380067e3ba
Richard Liu <xuegang.liu@nxp.com>
2023-10-18 22:08:59 +0800

    MA-21746 MediaInfo application on i.MX
    MediaInfo application helps to get media information.
    
    The source code is based on below commit, change target sdk to 23 and use Android.bp.
    commit f4d59a9b44a8c750ab32cf990814a019d29334a9 (android-imx/maddev_jb4.2.2_proprietary)
    Author: Zhu Yuqing <b48954@freescale.com>
    Date:   Mon Oct 20 10:17:36 2014 +0800
    
        MA-5768 create a new application to get media information from SD card.
    
        create a new application to get media information from SD card.
    
        Signed-off-by: Zhu Yuqing <b48954@freescale.com>
    
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Change-Id: I4c5496687a405073d20edf3012a2db01e4a6aabb
    Reviewed-on: http://androidsource.nxp.com/project/22911
    Reviewed-by: Mao Xu <mao.xu@nxp.com>
    Reviewed-by: Jindong Yue <jindong.yue@nxp.com>

---
+++ MediaInfo/src/com/example/mediainfo/ItemData.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (C) 2014 Freescale Semiconductor, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.mediainfo;
+
+public class ItemData {
+    public String mTitle;
+    public String mName;
+    public String mPath;
+    public String mMime;
+    public long   mDuration;
+    public String mArt;
+    public String mBucket;
+
+    public ItemData() {
+        mTitle = null;
+        mName  = null;
+        mPath  = null;
+        mMime  = null;
+        mDuration = 0;
+        mArt   = null;
+        mBucket = null;
+    }
+}
